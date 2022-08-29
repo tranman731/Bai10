3d34a09b2e8a76d0554726461d0bfa35a47bfd28
Man <tranman731@gmail.com>
2022-08-29 09:23:57 +0700

    first commit

---
+++ Bai10.js
@@ -0,0 +1,18 @@
+/**
+ * 10.1
+ */
+ function kt(n){
+    switch (n) {
+    case n > 0 :
+        console.log("n la so nguyen duong ")
+    break;
+    case n < 0 :
+         
+            console.log("n la so nguyen am ")
+        
+ }
+}
+kt(5)
+/**
+* Bai 9.2
+*/
